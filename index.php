844489a1576e195ff85e6c6978b93aa66a24cf4e
SrinivasBilla <srinivasbill12@gmail.com>
2024-09-23 10:42:09 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main template files
+ * @package Billa
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Billa theme</title>
+</head>
+<body>
+  
+</body>
+</html>
